a46e38408b1e3d7dcc372b3f97b00789d3be1a08
VUGAR <aghayev.vugar@gmail.com>
2018-05-20 15:17:44 +1000

    days in year calulator
    calculates the number of days in years between the input dates

--- src/Dool/DaysCalculator.java
+++ src/Dool/DaysCalculator.java
@@ -3,6 +3,7 @@ package Dool;
 public class DaysCalculator {
 	int[] start;
 	int[] end;
+	LeapYear leapYear;
 
 	/*
 	 * Constructor with two input arrays containing start and end dates
@@ -10,6 +11,7 @@ public class DaysCalculator {
 	public DaysCalculator(int[] start, int[] end) {
 		this.start = start;
 		this.end = end;
+		this.leapYear = new LeapYear();
 	}
 
 	/*
@@ -32,9 +34,20 @@ public class DaysCalculator {
 	/*
 	 * Calculates number of days between two given years
 	 */
-	public int calculatorForYears() {
-
-		return 0;
+	public int calculatorForYears(int start, int end) {
+		int theResult = end - start;
+		System.out.println("the result is "+theResult);
+		int theNumberOfDays = 0;
+		if (theResult > 1) {
+			for (int i = start + 1; i < end; i++) {
+				if (this.leapYear.leapYear(i)) {
+					theNumberOfDays += 366;
+				} else {
+					theNumberOfDays += 365;
+				}
+			}
+		}
+		return theNumberOfDays;
 	}
 
 	/*
@@ -49,6 +62,9 @@ public class DaysCalculator {
 	 * Calculates and returns the number of days between the dates
 	 */
 	public int calculateTheResult() {
+		// start[2] and end[2] are the years in the start and end arrays
+		int daysBetweenYears = this.calculatorForYears(this.start[2], this.end[2]);
+		System.out.println(daysBetweenYears);
 		return 0;
 	}
 }
