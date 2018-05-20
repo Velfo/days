f51be40a153821b2744f670193f7d52ed5ecad97
VUGAR <aghayev.vugar@gmail.com>
2018-05-20 15:48:23 +1000

    months
    calculating number of days in the months in the given year

--- src/Dool/DaysCalculator.java
+++ src/Dool/DaysCalculator.java
@@ -4,6 +4,7 @@ public class DaysCalculator {
 	int[] start;
 	int[] end;
 	LeapYear leapYear;
+	MonthsRange monthRange;
 
 	/*
 	 * Constructor with two input arrays containing start and end dates
@@ -12,6 +13,7 @@ public class DaysCalculator {
 		this.start = start;
 		this.end = end;
 		this.leapYear = new LeapYear();
+		this.monthRange = new MonthsRange();
 	}
 
 	/*
@@ -24,11 +26,18 @@ public class DaysCalculator {
 	}
 
 	/*
-	 * Calculates number of days between two given months
+	 * Calculates number of days between two given months in a specific year
 	 */
-	public int calculatorForMonths() {
-
-		return 0;
+	public int calculatorForMonths(int startMonth, int endMonth, int theYear) {
+		int theResult = endMonth - startMonth;
+		System.out.println("the result is " + theResult);
+		int theNumberOfDays = 0;
+		if (theResult > 1) {
+			for (int i = startMonth + 1; i < endMonth; i++) {
+				theNumberOfDays += this.monthRange.getTheExactDaysInMonth(i, theYear);
+			}
+		}
+		return theNumberOfDays;
 	}
 
 	/*
@@ -36,7 +45,7 @@ public class DaysCalculator {
 	 */
 	public int calculatorForYears(int start, int end) {
 		int theResult = end - start;
-		System.out.println("the result is "+theResult);
+		System.out.println("the result is " + theResult);
 		int theNumberOfDays = 0;
 		if (theResult > 1) {
 			for (int i = start + 1; i < end; i++) {
@@ -64,6 +73,10 @@ public class DaysCalculator {
 	public int calculateTheResult() {
 		// start[2] and end[2] are the years in the start and end arrays
 		int daysBetweenYears = this.calculatorForYears(this.start[2], this.end[2]);
+		// start[1] and end[1] are the months in the start and end arrays
+		int daysBetweenMonthsFirstPart = this.calculatorForMonths(this.start[1], 12, this.start[2]);
+		int daysBetweenMonthsSecondPart = this.calculatorForMonths(0, this.end[1], this.end[2]);
+		
 		System.out.println(daysBetweenYears);
 		return 0;
 	}
--- src/Dool/MonthsRange.java
+++ src/Dool/MonthsRange.java
@@ -38,6 +38,40 @@ public class MonthsRange {
 		return false;
 	}
 	/*
+	 * Get the exact number of days in a given month
+	 */
+	public int getTheExactDaysInMonth(int theMonth, int theYear) {
+		int thirtyOneDays[] = { 1, 3, 5, 7, 8, 10, 12 };
+		int thirtyDays[] = { 4, 6, 9, 11 };
+		int february = 2;
+
+		// check if month is 31 days
+		for (int i : thirtyOneDays) {
+			if (i == theMonth) {
+				return 31;
+			}	
+		}
+		// check if month is 30 days
+		for (int i : thirtyDays) {
+			if (i == theMonth) {
+				return 30;
+			}
+		}
+		// check for February
+		if (theMonth == february) {
+			// if the year is leap year
+			LeapYear leapYear = new LeapYear();
+			boolean itIsLeapYear = leapYear.leapYear(theYear);
+
+			if (itIsLeapYear) {
+				return 29;
+			} else if (!itIsLeapYear) {
+				return 28;
+			}
+		}
+		return 0;
+	}
+	/*
 	 * Finding out if the integer is between the range.
 	 */
 	public boolean betweenNums(int i, int minValueInclusive, int maxValueInclusive) {
